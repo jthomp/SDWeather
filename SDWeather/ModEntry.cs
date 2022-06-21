27bdfc8a7d94f85a95c2e60484063314125da740
Justin Thompson <jthomp1286@gmail.com>
2022-06-21 16:46:08 -0400

    update version update .net framework bump version

--- SDWeather/ModEntry.cs
+++ SDWeather/ModEntry.cs
@@ -1,8 +1,21 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿/***************************************************************************************************************************************
+ *                                                                                                                                     *
+ * SDWeather                                                                                                                           *
+ * By: Justin Thompson                                                                                                                 *
+ * A tiny mod to show you today's weather and tomorrow's luck when the day starts.                                                     *
+ * Copyright (C) Justin Thompson. All Rights Reserved.                                                                                 *
+ *                                                                                                                                     *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,                                                 *
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.               *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,                             *
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                                                                  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                             * 
+ *                                                                                                                                     *
+ ***************************************************************************************************************************************
+*/
+
+using System;
 using StardewModdingAPI;
-using StardewModdingAPI.Events;
-using StardewModdingAPI.Utilities;
 using StardewValley;
 
 namespace SDWeather {
